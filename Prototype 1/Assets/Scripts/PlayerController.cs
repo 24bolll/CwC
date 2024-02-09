1f0cf3a2da01d71f035d42d6093c9ce17b091fba
24bolll <124065255+24bolll@users.noreply.github.com>
2024-02-09 09:15:11 -0500

    Challenge PARTIAL

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -10,17 +10,19 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    public float speed = 20;
-    public float turnspeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20;
+    private float turnspeed = 45;
+    private float horizontalInput;
+    private float forwardInput;
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
             forwardInput = Input.GetAxis("Vertical");
 
+        // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
     }
 }
