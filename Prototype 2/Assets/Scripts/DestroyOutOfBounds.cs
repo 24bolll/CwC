82cc3ace095e89b5b251e0e3f9af6293e5dfade0
24bolll <124065255+24bolll@users.noreply.github.com>
2024-02-22 08:29:43 -0500

    2.2 out of bounds script is being a pain

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,12 +7,16 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+    private float topBound = 30;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
     }
-}
+    
